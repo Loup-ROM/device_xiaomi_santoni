81811ce598ffc303cf82041c3c555175ada5ce6a
bitrvmpd <e.noyer.silva@gmail.com>
2020-06-11 12:19:27 -0500

    :construction: Added missing Snap cam permissions. Revert later.

--- device.mk
+++ device.mk
@@ -521,3 +521,7 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGES += \
     XiaomiParts \
     XiaomiAddon
+
+# Privapp permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-lineage.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lineage.xml
